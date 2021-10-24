03bdffeaceb142286a8979f02460aa93986bbc2c
Randal Root - Personal Acct <Randal.Root@hotmail.com>
2021-10-23 18:20:19 -0700

    Add files via upload

---
+++ codefiles/demo01.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------#
+# Title: A Simple Test Script
+# Dev:   RRoot
+# Date:  Oct 24, 2021
+# ChangeLog: (Who, When, What)
+#   
+#-------------------------------------------------#
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("You entered:", first_name, last_name)
+# or
+print("You entered: " + first_name + ' ' + last_name, '\n')
