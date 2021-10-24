b94d4da29da809d02b3452a32b366907ad0e2f4e
Randal Root - Personal Acct <Randal.Root@hotmail.com>
2021-10-23 18:30:14 -0700

    Update demo01.py

--- codefiles/demo01.py
+++ codefiles/demo01.py
@@ -1,6 +1,3 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
-
 print("You entered:", first_name, last_name)
-# or
-print("You entered: " + first_name + ' ' + last_name, '\n')
